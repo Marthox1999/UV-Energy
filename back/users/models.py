f932ee20265af63edf93b9ec8c4c354733bd1eaa
Marthox1999 <mateo.gregory@correounivalle.edu.co>
2020-02-11 10:07:33 -0500

    intento de solucion problema mac

--- back/users/models.py
+++ back/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    #Abstract user fields
+    # 1) numero de documento 2) date_joined 3)email
+    # 4) fecha_nacimiento
+    class Meta:
+        ordering = ['first_name','last_name']
+
+    def __str__(self):
+        return self.get_full_name()
+
+    cedula = models.CharField(unique=True, max_length=20)
+
+    ADMINISTRADOR = 'administrador'
+    GERENTE = 'gerente'
+    OPERADOR = 'operador'
+    JEFE = 'jefe'
