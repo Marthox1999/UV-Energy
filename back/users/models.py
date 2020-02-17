e11535ceef3b13843e5ae776841fca98ca18dcad
emyesme <emily.carvajal@correounivalle.edu.co>
2020-02-17 11:37:56 -0500

    implementing database

--- back/users/models.py
+++ back/users/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    # Abstract user fields
-    # 1) numero de documento 2) date_joined 3)email
-    # 4) fecha_nacimiento
-    class Meta:
-        ordering = ['first_name','last_name']
+class Client(models.Model):
+    cc = models.CharField(unique=True, max_length=20)
 
-    def __str__(self):
-        return self.get_full_name()
+# Employee
 
-    cedula = models.CharField(unique=True, max_length=20)
 
-    ADMINISTRADOR = 'administrador'
-    GERENTE = 'gerente'
-    OPERADOR = 'operador'
-    JEFE = 'jefe'
+class Employee(AbstractUser):
+    POSITION = {
+        ('MGR', 'Manager'),
+        ('ADMIN', 'Administrator'),
+        ('OP', 'Operator')
+    }
+    pk_employee = models.CharField(unique=True,
+                                   max_length=10,
+                                   primary_key=True)
+    position = models.CharField(max_length=5, choices=POSITION, default='OP')
