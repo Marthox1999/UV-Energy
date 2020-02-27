cf711e5e8d69d748a891cf6166552f15f579af4e
Marthox1999 <mateo.gregory@correounivalle.edu.co>
2020-02-27 12:45:08 -0500

    finished and lee proved authentication functions

--- back/UVEnergyBack/urls.py
+++ back/UVEnergyBack/urls.py
@@ -6,6 +6,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from users import api as api_users
 
 urlpatterns = [
-    path('api-token-auth/', obtain_auth_token),
-    path('api-user/', api_users.APIUser.as_view()),
+    path('api/users/auth/', obtain_auth_token),
+    path('api/users/', include('users.urls')),
 ]
--- back/users/api.py
+++ back/users/api.py
@@ -1,9 +1,12 @@
 from .models import User
-from rest_framework import viewsets, permissions, generics
+from rest_framework import viewsets, permissions
 from .serializers import UserSerializer
 
 
 # User ViewSet
-class APIUser (generics.ListCreateAPIView):
+class UserViewSet (viewsets.ModelViewSet):
     queryset = User.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
     serializer_class = UserSerializer
