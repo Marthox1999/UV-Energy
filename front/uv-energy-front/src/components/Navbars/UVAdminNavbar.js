f8a3b41dddd8a74fb76534164253730924c1ec57
Marthox1999 <mateo.gregory@correounivalle.edu.co>
2020-03-30 15:51:54 -0500

    terminado logout en dropdown

--- front/uv-energy-front/src/components/Navbars/UVAdminNavbar.js
+++ front/uv-energy-front/src/components/Navbars/UVAdminNavbar.js
@@ -17,7 +17,6 @@ const cookie = new Cookies();
 
 class UVAdminNavbar extends React.Component {
   handlelogout = (event) => {
-    event.preventDefault()
     cookie.remove('notCredentials', { path: '/' })
   }
   render() {
@@ -51,24 +50,22 @@ class UVAdminNavbar extends React.Component {
                   <DropdownItem className="noti-title" header tag="div">
                     <h6 className="text-overflow m-0">Welcome!</h6>
                   </DropdownItem>
-                  <DropdownItem to="/admin/user-profile">
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-single-02" />
                     <span>My profile</span>
                   </DropdownItem>
-                  <DropdownItem to="/admin/user-profile">
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-settings-gear-65" />
                     <span>Settings</span>
                   </DropdownItem> 
-                  <DropdownItem to="/admin/user-profile">
+                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-support-16" />
                     <span>Support</span>
                   </DropdownItem>
                   <DropdownItem divider />
-                  <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
+                  <DropdownItem to="/login" tag={Link} onClick={this.handlelogout}>
                     <i className="ni ni-user-run" />
-                    <span onClick={this.handlelogout}>
-                      <Link to="/login">Logout</Link>
-                    </span>
+                    <span>Logout</span>
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
