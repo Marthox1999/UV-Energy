f9810a96b86014246c74d53f1307e06102d87bdc
emyesme <emily.carvajal@correounivalle.edu.co>
2020-05-11 19:55:51 -0500

    generated invoices

--- back/sales/api.py
+++ back/sales/api.py
@@ -29,7 +29,8 @@ class GeneratePDFViewSet(viewsets.ViewSet):
         meterJson = json.loads(serializers.serialize('json',[meter,]))
         clientJson = json.loads(serializers.serialize('json',[client,]))
         # guardarlo en un contexto
-        context = { "bill" : billJson[0], "meter" : meterJson[0], "client" : clientJson[0]}
+        print(billJson[0]['fields']['value'])
+        context = { "bill" : billJson[0], "meter" : meterJson[0], "client" : clientJson[0], "unit" : round(billJson[0]['fields']['value'] / billJson[0]['fields']['read'],3)}
         # busca el template a utilizar
         template = get_template("bill.html")
         # llena el template con la informacion que se recupero de la factura
@@ -58,9 +59,7 @@ class GenerateBillsViewSet(viewsets.ViewSet):
         # obtengo la informacion entrante    
         residencial = request.data["residencial"]
         industrial = request.data["industrial"]
-        mora = request.data["mora"]
-        reconexion = request.data["reconexion"]
-        # print(residencial)
+        mora = float(request.data["mora"])
         # cantidad de meters activos
         meters = Meter.objects.filter( isActive = True)
         aux = Bill.objects.filter(fk_meter_id=meters[0].pk_meter).order_by('-end_date').first()
@@ -69,10 +68,10 @@ class GenerateBillsViewSet(viewsets.ViewSet):
         new_end_date = new_start_date + timedelta(days=30)
         new_expedition_date = datetime.now().date()
         new_expiration_date = new_expedition_date + timedelta(days=15)
-        print("new_start_date ", new_start_date)
-        print("new_end_date ", new_end_date)
-        print("new_expedition_date ", new_expedition_date)
-        print("new_expiration_date ", new_expiration_date)
+        # print("new_start_date ", new_start_date)
+        # print("new_end_date ", new_end_date)
+        # print("new_expedition_date ", new_expedition_date)
+        # print("new_expiration_date ", new_expiration_date)
         # tomo la ultima factura de cada meter (suponiendo uno cada uno)
         for meter in meters:
             # tomo la ultima factura del contador 
@@ -86,8 +85,10 @@ class GenerateBillsViewSet(viewsets.ViewSet):
             # lectura de consumo
             read = randint(400,1000)
             # si no hay bill es nuevo genero normal
+            print("meter: ", meter.pk_meter)
             if bill is None:      
                 # meters que no tiene factura solo genero
+                print("# meters que no tiene factura solo genero")
                 aux = Bill(
                     fk_meter = meter,
                     fk_debit_payment = None,
@@ -102,11 +103,22 @@ class GenerateBillsViewSet(viewsets.ViewSet):
                 )
             else:
                 # si la factura no esta pagada 
+                print("# si la factura no esta pagada :", bill.is_paid)
                 if not bill.is_paid:
                     # busco la anterior a ella
                     secondbill = Bill.objects.filter(fk_meter_id=meter.pk_meter).order_by('-end_date')
+                    print("# busco la anterior a ella :", secondbill.count() == 1)
                     if secondbill.count() == 1:
+                        print("# solo existe un recibo y no se a pagado genero mora")
                         # solo existe un recibo y se no se apago genero mora
+                        print("tiempo mora--solo uno --", datetime.now().date() - bill.expiration_date)
+                        now =  datetime.now().date().year*1000 + datetime.now().date().month*100 + datetime.now().date().day
+                        past = bill.expiration_date.year*1000 + bill.expiration_date.month*100 + bill.expiration_date.day
+                        print("tiempo mora--anterior pagada", now - past)
+                        if (abs(now - past) >= 30):
+                            interest = bill.value * 0.3
+                        else:
+                            interest = bill.value * abs(now - past)* mora + bill.value
                         aux = Bill(
                             fk_meter = meter,
                             fk_debit_payment = None,
@@ -116,26 +128,27 @@ class GenerateBillsViewSet(viewsets.ViewSet):
                             expedition_date = new_expedition_date,
                             expiration_date = new_expiration_date,
                             is_paid = False,
-                            value = int(valorUnitario * read), ####################
+                            value = int(valorUnitario * read) + interest, ####################
                             read = read,
                         )
                     else:
+                        print("hace dos meses no se pago: ", secondbill[1].is_paid)
                         if not secondbill[1].is_paid:
+                            print("# si la anterior a ella no esta apagada genero corte")
                             # si la anterior a ella no esta apagada genero corte
-                            aux = Bill(
-                                fk_meter = meter,
-                                fk_debit_payment = None,
-                                fk_employee = None,
-                                start_date = new_start_date,
-                                end_date = new_end_date,
-                                expedition_date = new_expedition_date,
-                                expiration_date = new_expiration_date,
-                                is_paid = False,
-                                value = int(valorUnitario * read),####################
-                                read = read,
-                            )
+                            # no se genera factur para este
+                            print("anterior de anterior no pagada corte--------no genera")
+                            
                         else:
+                            print("# si el hace 2 meses esta pagada genero mora")
                             # si esta pagada genero mora
+                            now =  datetime.now().date().year*1000 + datetime.now().date().month*100 +datetime.now().date().day
+                            past = bill.expiration_date.year*1000 + bill.expiration_date.month*100 + bill.expiration_date.day
+                            print("tiempo mora--anterior pagada", now - past)
+                            if (abs(now - past) >= 30):
+                                interest = bill.value * 0.3
+                            else:
+                                interest = bill.value * abs(now - past)* mora + bill.value
                             aux = Bill(
                                 fk_meter = meter,
                                 fk_debit_payment = None,
@@ -145,10 +158,11 @@ class GenerateBillsViewSet(viewsets.ViewSet):
                                 expedition_date = new_expedition_date,
                                 expiration_date = new_expiration_date,
                                 is_paid = False,
-                                value = int(valorUnitario * read), ####################
+                                value = int(valorUnitario * read) + interest, ####################
                                 read = read,
                             )
                 else:
+                    print("# si la encontrada esta pagada")
                     # si la encontrada esta pagada
                     # genero una normal
                     aux = Bill(
@@ -166,9 +180,9 @@ class GenerateBillsViewSet(viewsets.ViewSet):
             try:
                 aux.full_clean()
             except ValidationError as e:
-                print("error de validacion de datos de generacion automatica !!!!!!!!!!!!!!", e)
+                print("error de validacion de datos de generacion automatica !", e)
             aux.save()
-            print("biennnnnnnnnnnnnnnnnnnnnnnnn")
+        print("biennnnnnnnnnnnnnnnnnnnnnnnn")
         return Response("Las facturas se han generado correctamente")
 
 
@@ -197,8 +211,6 @@ class PaidBillListViewSet (viewsets.ViewSet):
         meter_ids=[]
         for meter in meters:
             meter_ids.append(meter.pk_meter)
-            print(meter.pk_meter)
-        print(meter)
         queryset = Bill.objects.filter(Q(is_paid=True), Q(fk_meter__in=meter_ids)).order_by('-end_date')
         serializer = BillSerializers(queryset, many=True)
         return Response(serializer.data)
@@ -213,8 +225,6 @@ class PendingBillListViewSet (viewsets.ViewSet):
         meter_ids=[]
         for meter in meters:
             meter_ids.append(meter.pk_meter)
-            print(meter.pk_meter)
-        print(meter)
         queryset = Bill.objects.filter(Q(is_paid=False), Q(fk_meter__in=meter_ids)).order_by('-end_date')
         serializer = BillSerializers(queryset, many=True)
         return Response(serializer.data)
